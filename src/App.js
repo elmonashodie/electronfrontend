f56f722ca32b750935321dbdb3ad02f29171e041
Elmona <emil_larsson83@hotmail.com>
2019-06-10 13:06:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Alma</h1>
+    </div>
+  )
+}
+
+export default App
